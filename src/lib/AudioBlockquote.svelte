73205a09292af0fa782e373047f93a7f4328596d
Yosua Ian Sebastian <developer@darcien.dev>
2021-04-17 01:28:12 +0700

    add initial layout

---
+++ src/lib/AudioBlockquote.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+	export let author: string = '紫咲シオン';
+	export let content: string;
+	export let cite: string;
+	export let date: string;
+	export let src: string;
+
+	let audio: HTMLAudioElement;
+	let paused = true;
+
+	function stopOthers() {
+		// TODO: implement stopOthers
+	}
+
+	function playPause() {
+		if (paused) {
+			audio.play();
+		} else {
+			audio.pause();
+		}
+	}
+</script>
+
+<figure on:click={playPause}>
+	<blockquote {cite}>
+		<h2>{content}</h2>
+	</blockquote>
+	<!-- svelte-ignore a11y-media-has-caption -->
+	<audio bind:this={audio} bind:paused on:play={stopOthers} {src} />
+	<figcaption>—{author}, {date}. <cite><a href={cite}>Twitter</a></cite>.</figcaption>
+</figure>
+
+<style>
+	figure {
+		text-align: left;
+		border-left: 3px solid rgb(214, 193, 217);
+		padding: 2em;
+		margin-bottom: 2em;
+		border-bottom-right-radius: 1em;
+	}
+
+	figure:focus,
+	figure:hover {
+		border-top: 2px solid rgb(214, 193, 217);
+		border-left: 5px solid rgb(214, 193, 217);
+		cursor: pointer;
+	}
+
+	figure:active {
+		background-color: rgba(214, 193, 217, 0.2);
+	}
+
+	blockquote {
+		color: rgb(237, 186, 132);
+		margin: 0;
+	}
+
+	a {
+		color: rgb(160, 86, 103);
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,49 +1,53 @@
 <script lang="ts">
-	import Counter from '$lib/Counter.svelte';
+	import AudioBlockquote from '$lib/AudioBlockquote.svelte';
 </script>
 
 <svelte:head>
-	<title>Hello world!</title>
+	<title>Murasaki — Dedicated for a certain cheeky magician!</title>
 </svelte:head>
 
 <main>
-	<h1>Hello world!</h1>
-
-	<Counter />
-
-	<p>Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.</p>
+	<h1>紫咲　→　むらさき　→　Murasaki</h1>
+	<AudioBlockquote
+		content="紫咲シオン超かわい～～！！"
+		cite="https://twitter.com/murasakishionch/status/1380159987582955526"
+		src="https://res.cloudinary.com/dm7sc2usi/video/upload/v1618595017/murasaki/oyasumi.mp3"
+		date="2021-04-08"
+	/>
+	<AudioBlockquote
+		content="リテイクしました"
+		cite="https://twitter.com/murasakishionch/status/1371008509345599493"
+		src="https://res.cloudinary.com/dm7sc2usi/video/upload/v1618596566/murasaki/white_day_retake.mp3"
+		date="2021-03-14"
+	/>
+	<AudioBlockquote
+		content="ハッピーホワイトデー🍫💕 お返し倍でよこせ！！"
+		cite="https://twitter.com/murasakishionch/status/1371007610753716224"
+		src="https://res.cloudinary.com/dm7sc2usi/video/upload/v1618596566/murasaki/white_day.mp3"
+		date="2021-03-14"
+	/>
+	<AudioBlockquote
+		content="改めて初ボイス機能💭 おやすみ😪"
+		cite="https://twitter.com/murasakishionch/status/1330165037105881088"
+		src="https://res.cloudinary.com/dm7sc2usi/video/upload/v1618596566/murasaki/oyasumi_ancient.mp3"
+		date="2020-11-21"
+	/>
 </main>
 
 <style>
 	main {
 		text-align: center;
 		padding: 1em;
-		margin: 0 auto;
+		max-width: 1080px;
+		margin: 2rem auto;
 	}
 
 	h1 {
-		color: #ff3e00;
+		color: rgb(214, 193, 217);
 		text-transform: uppercase;
 		font-size: 4rem;
 		font-weight: 100;
 		line-height: 1.1;
-		margin: 4rem auto;
-		max-width: 14rem;
-	}
-
-	p {
-		max-width: 14rem;
-		margin: 2rem auto;
-		line-height: 1.35;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			max-width: none;
-		}
-
-		p {
-			max-width: none;
-		}
+		margin-bottom: 1.2em;
 	}
 </style>
